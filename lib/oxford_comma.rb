d6b8877655291ea5dd6512d4b4714792627c53e7
Nethelbert <gilblasse@gmail.com>
2019-11-04 18:21:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 require 'pry'
 
 def oxford_comma(array)
-  array.each_with_object([]) do |item,a|
-    
-    if a.size > 0 && a.size == (a.index(a.last) + 1)
-      a << "and #{item}"
-    else
-      a << item
-    end
-
-  end
-  
+  arr = array.each_with_object([]) do |item,a|
+      
+          if a.size > 0 && a.size == (a.index(a.last) + 1)
+            a << "and #{item}"
+          else
+            a << item
+          end
+      
+        end
+  arr.join(',')
 end
