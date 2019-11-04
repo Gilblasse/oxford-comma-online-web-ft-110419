0e2fb1a8352009447fd5b0e656ba79174e7d24fc
Nethelbert <gilblasse@gmail.com>
2019-11-04 17:25:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,3 @@
 def oxford_comma(array)
-  if array.length == 1
-    array[0]
-  elsif array.length == 2
-    "#{array[0]} and #{array[1]}"
-  elsif array.length == 3
-    "#{array[0]}, #{array[1]}, and #{array[2]}"
-  end
+  array.reduce {|memo,i| }
 end
