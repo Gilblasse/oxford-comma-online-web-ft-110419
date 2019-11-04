8f079441efa238a536ae4f25d698fc453d2168f3
Nethelbert <gilblasse@gmail.com>
2019-11-04 17:21:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,5 @@ def oxford_comma(array)
   elsif array.length == 2
     "#{array[0]} and #{array[1]}"
   elsif array.length == 3
-    "#{array[0]} and #{array[1]}"
+    "#{array[0]} and #{array[1]} and #{array[2]}"
 end
