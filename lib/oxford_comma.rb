182f05777dba9cb6411abbf7ed84912889809776
Nethelbert <gilblasse@gmail.com>
2019-11-04 17:47:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 require 'pry'
 
 def oxford_comma(array)
-  binding.pry
-  arr = 
+  arr = array.map {|item| item}
   if array.index(array.first) != array.index(array.last)
     arr << ",and #{arr.pop}"
   end
