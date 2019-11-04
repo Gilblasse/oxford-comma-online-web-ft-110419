0674b0e93ffbef490cc69e71445c6e559d2bd39a
Nethelbert <gilblasse@gmail.com>
2019-11-04 17:57:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def oxford_comma(array)
-  if array.index(array.first) != array.index(array.last) && array.index(array.last) + 1
+  if array.index(array.first) != array.index(array.last) && array.size == (array.index(array.last) + 1)
     array << "and #{arr.pop}"
   end
  array.join(',')
