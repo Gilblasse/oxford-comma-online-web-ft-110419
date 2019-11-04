3ea8721deb7b5c4ad0c4095405bb37701a62460b
Nethelbert <gilblasse@gmail.com>
2019-11-04 18:14:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ require 'pry'
 def oxford_comma(array)
   array.each_with_object([]) do |item,a|
     
-    if a.size > 1 && a.size == (a.index(a.last) + 1)
+    if a.size > 0 && a.size == (a.index(a.last) + 1)
        a << "and #{a.pop}"
     else
       a << item
